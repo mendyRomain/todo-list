5b1ca1f9bba1ac651554d102f40450f72f613eed
mendy.romain <mendy.romain@gmail.com>
2020-04-15 21:59:28 +0200

    la correction du chef (j'ai encore éspoir de t'apprendre un jour )

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 
+@Injectable({
+    providedIn: 'root'
+  })
 export class AuthService {
-
-    @Injectable({
-        providedIn: 'root'
-      })
     
     isAuth:boolean = false;
   
